66b1ad4dd6d6bb5c0ae433ba5b57a861e3af8063
Bruce Andrada <88586952+bmpandrada@users.noreply.github.com>
2021-10-21 21:33:02 +0800

    Add files via upload
    simple project

---
+++ script.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    var yourName = prompt("what is your name?");
+    var theirName = prompt("what is their name?");
+
+
+    var loveScore = Math.random() * 100;
+    loveScore = Math.floor(loveScore) + 1;
+
+
+    if (loveScore > 70) {
+        demo.innerHTML = "your love score " + yourName + " from " + theirName + loveScore + "%" + " you love each other.";
+    } else {
+        demo.innerHTML = theirName + " love score " + loveScore + "% Find out what your crush is passionate about.";
+    }
+
+
+
+
+}
